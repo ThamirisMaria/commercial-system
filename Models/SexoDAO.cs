b75a7a0f397628fb4421659fb772a95b777c88bd
Thamiris Maria <thamirismariasouzadeoliveira@gmail.com>
2021-11-21 02:28:19 -0300

    Add class SexoDAO: implements IDAO interface in Sexo

---
+++ Models/SexoDAO.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using SistemaVendas.Database;
+using SistemaVendas.Interfaces;
+
+namespace SistemaVendas.Models
+{
+    class SexoDAO : IDAO<Sexo>
+    {
+        public void Delete(Sexo t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Sexo GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(Sexo t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Sexo> List()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Sexo t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
