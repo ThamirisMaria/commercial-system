193fb407c06fa1c3c1200ea45a4ddefb3bb0245c
Thamiris Maria <thamirismariasouzadeoliveira@gmail.com>
2021-11-21 02:53:08 -0300

    Add class ClienteDAO: implements IDAO interface in Cliente

---
+++ Models/ClienteDAO.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SistemaVendas.Interfaces;
+
+namespace SistemaVendas.Models
+{
+    class ClienteDAO : IDAO<Cliente>
+    {
+        public void Delete(Cliente t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Cliente GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(Cliente t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Cliente> List()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Cliente t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
