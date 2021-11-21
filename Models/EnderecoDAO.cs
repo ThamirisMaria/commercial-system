f817489edce987f7878fbc66f81325e989ac177c
Thamiris Maria <thamirismariasouzadeoliveira@gmail.com>
2021-11-21 02:33:49 -0300

    Add class EnderecoDAO: implements IDAO interface in Endereco

---
+++ Models/EnderecoDAO.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SistemaVendas.Database;
+using SistemaVendas.Interfaces;
+
+namespace SistemaVendas.Models
+{
+    class EnderecoDAO : IDAO<Endereco>
+    {
+        public void Delete(Endereco t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Endereco GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(Endereco t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Endereco> List()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Endereco t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
