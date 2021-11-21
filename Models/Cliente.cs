47732a622638ad313cda33b00c47006d49e03d8b
Thamiris Maria <thamirismariasouzadeoliveira@gmail.com>
2021-11-21 02:50:04 -0300

    Add class Cliente

---
+++ Models/Cliente.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaVendas.Models
+{
+    class Cliente
+    {
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public string CPF { get; set; }
+
+        public string RG { get; set; }
+
+        public DateTime DataNascimento { get; set; }
+
+        public string Telefone { get; set; }
+
+        public string Celular { get; set; }
+
+        public string Email { get; set; }
+
+        public Sexo Sexo { get; set; }
+
+        public Endereco Endereco { get; set; }
+    }
+}
