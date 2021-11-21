b470df18fb232c6956e51cbb44141b35a9da83d8
Thamiris Maria <thamirismariasouzadeoliveira@gmail.com>
2021-11-21 16:02:37 -0300

    Update class FuncionarioDAO: connects FuncionarioDAO to the database

--- Models/FuncionarioDAO.cs
+++ Models/FuncionarioDAO.cs
@@ -4,11 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using SistemaVendas.Interfaces;
+using SistemaVendas.Database;
 
 namespace SistemaVendas.Models
 {
     class FuncionarioDAO : IDAO<Funcionario>
     {
+        private static Conexao conexao;
+
+        public FuncionarioDAO()
+        {
+            conexao = new Conexao();
+        }
         public void Delete(Funcionario t)
         {
             throw new NotImplementedException();
