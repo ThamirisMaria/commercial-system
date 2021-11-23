1f93634c5c5d6562c8523b484236263df3d08dd5
Thamiris Maria <thamirismariasouzadeoliveira@gmail.com>
2021-11-23 02:28:05 -0300

    Update class ClienteDAO: Adds "insert" functionality to Cliente

--- Models/ClienteDAO.cs
+++ Models/ClienteDAO.cs
@@ -4,11 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using SistemaVendas.Interfaces;
+using SistemaVendas.Database;
 
 namespace SistemaVendas.Models
 {
     class ClienteDAO : IDAO<Cliente>
     {
+        private static Conexao conexao;
+
         public void Delete(Cliente t)
         {
             throw new NotImplementedException();
@@ -21,7 +24,35 @@ namespace SistemaVendas.Models
 
         public void Insert(Cliente t)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var query = conexao.Query();
+                query.CommandText = "INSERT INTO cliente (nome_cli, cpf_cli, rg_cli, datanasc_cli, telefone_fixo_cli, telefone_celular_cli, email_cli, cod_sex_fk, cod_end_fk) " +
+                    "VALUES (@nome, @cpf, @rg, @datanasc, @telefone, @celular, @email, @sexoId, @enderecoId)";
+
+                query.Parameters.AddWithValue("@nome", t.Nome);
+                query.Parameters.AddWithValue("@cpf", t.CPF);
+                query.Parameters.AddWithValue("@rg", t.RG);
+                query.Parameters.AddWithValue("@datanasc", t.DataNascimento.ToString("yyyy-MM-dd"));
+                query.Parameters.AddWithValue("@telefone", t.Telefone);
+                query.Parameters.AddWithValue("@celular", t.Celular);
+                query.Parameters.AddWithValue("@email", t.Email);
+                query.Parameters.AddWithValue("@sexoId", t.Sexo.Id);
+                query.Parameters.AddWithValue("@enderecoId", t.Endereco.Id);
+
+                var linhasAfetadas = query.ExecuteNonQuery();
+
+                if (linhasAfetadas == 0)
+                    throw new Exception("Registro não efetuado. Verifique e tente novamente.");
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            finally
+            {
+                conexao.Close();
+            }
         }
 
         public List<Cliente> List()
