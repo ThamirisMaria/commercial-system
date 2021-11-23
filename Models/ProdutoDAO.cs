992842ce448d28f6b137ffb32377f76c2dffb7e3
Thamiris Maria <thamirismariasouzadeoliveira@gmail.com>
2021-11-23 02:23:00 -0300

    Add class ProdutoDAO: implements IDAO interface in Produto

---
+++ Models/ProdutoDAO.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SistemaVendas.Interfaces;
+
+namespace SistemaVendas.Models
+{
+    class ProdutoDAO : IDAO<Produto>
+    {
+        public void Delete(Produto t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Produto GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(Produto t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Produto> List()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Produto t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
