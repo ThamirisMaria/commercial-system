e9a56baa0b0db3324a5550d75745c4b3e3a6a448
Thamiris Maria <thamirismariasouzadeoliveira@gmail.com>
2022-07-03 14:31:36 -0300

    Update window MainWindow

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -31,13 +31,49 @@ namespace SistemaVendas
 
         private void MainWindow_Loaded(object sender, RoutedEventArgs e)
         {
-            try
-            {
-                var conexao = new Conexao();
-            } catch(Exception exception)
-            {
-                MessageBox.Show(exception.Message);
-            }
+            
+        }
+
+        private void clientes_menu_btn_Click(object sender, RoutedEventArgs e)
+        {
+            ClienteWindow clienteWindow = new ClienteWindow();
+            clienteWindow.ShowDialog();
+        }
+
+        private void funcionarios_menu_btn_Click(object sender, RoutedEventArgs e)
+        {
+            FuncionarioWindow funcionarioWindow = new FuncionarioWindow();
+            funcionarioWindow.ShowDialog();
+        }
+
+        private void produtos_menu_btn_Click(object sender, RoutedEventArgs e)
+        {
+            ProdutoWindow produtoWindow = new ProdutoWindow();
+            produtoWindow.ShowDialog();
+        }
+
+        private void vendas_menu_btn_Click(object sender, RoutedEventArgs e)
+        {
+            VendaWindow vendaWindow = new VendaWindow();
+            vendaWindow.ShowDialog();
+        }
+
+        private void vender_btn_Click(object sender, RoutedEventArgs e)
+        {
+            CadastroVendaWindow cadastroVendaWindow = new CadastroVendaWindow();
+            cadastroVendaWindow.ShowDialog();
+        }
+
+        private void cadastrar_btn_Click(object sender, RoutedEventArgs e)
+        {
+            CadastroGeralWindow cadastroGeralWindow = new CadastroGeralWindow();
+            cadastroGeralWindow.ShowDialog();
+        }
+
+        private void dashboard_menu_btn_Click(object sender, RoutedEventArgs e)
+        {
+            DashboardWindow dashboardWindow = new DashboardWindow();
+            dashboardWindow.ShowDialog();
         }
     }
 }
