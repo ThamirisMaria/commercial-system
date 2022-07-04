c57e3270a22f079fdb82cd471f826f91671eb43f
Thamiris Maria <thamirismariasouzadeoliveira@gmail.com>
2022-07-04 20:21:27 -0300

    Add class Venda

---
+++ Models/Venda.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaVendas.Models
+{
+    class Venda
+    {
+        public int Id { get; set; }
+
+        public DateTime Data { get; set; }
+
+        public string FormaPagamento { get; set; }
+
+        public double ValorTotal { get; set; }
+
+        public Funcionario Funcionario { get; set; }
+
+        public Cliente Cliente { get; set; }
+
+        public List<VendaItem> Itens { get; set; } = new List<VendaItem>();
+    }
+}
